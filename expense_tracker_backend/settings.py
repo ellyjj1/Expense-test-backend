65d4f910fff9311e61256578b5296935718f24dc
ellyjj1 <72015673+ellyjj1@users.noreply.github.com>
2024-09-30 12:09:36 +1300

    update startup.sh

--- expense_tracker_backend/settings.py
+++ expense_tracker_backend/settings.py
@@ -73,6 +73,7 @@ DATABASES = {
     'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
 }
 
+
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
--- startup.sh
+++ startup.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 python manage.py migrate
 python manage.py collectstatic --noinput
+echo "Migration completed"
